db0d4f975ab4029704793ed0ddb0bb5073411079
Sébastiaan <se_bastiaan@outlook.com>
2021-12-05 16:55:16 +0100

    Move partner events to events package as 'external events'

--- website/events/api/calendarjs/views.py
+++ website/events/api/calendarjs/views.py
@@ -4,41 +4,62 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from events.api.calendarjs.serializers import (
     EventsCalenderJSSerializer,
     UnpublishedEventsCalenderJSSerializer,
+    ExternalEventCalendarJSSerializer,
 )
 from events.api.calendarjs.permissions import UnpublishedEventPermissions
+from events.api.v2 import filters
 from events.models import Event
+from events.models.external_event import ExternalEvent
 from utils.snippets import extract_date_range
 
 
 class CalendarJSEventListView(ListAPIView):
     """Define a custom route that outputs the correctly formatted events information for CalendarJS, published events only."""
 
+    queryset = Event.objects.filter(published=True)
     serializer_class = EventsCalenderJSSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     pagination_class = None
+    filter_backends = (
+        filters.EventDateFilter,
+        filters.CategoryFilter,
+        filters.OrganiserFilter,
+    )
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         context["member"] = self.request.member
         return context
-
-    def get_queryset(self):
-        start, end = extract_date_range(self.request)
-        return Event.objects.filter(end__gte=start, start__lte=end, published=True)
 
 
 class CalendarJSUnpublishedEventListView(ListAPIView):
-    """Define a custom route that outputs the correctly formatted events information for CalendarJS, unpublished events only."""
+    """Define a custom route that outputs the correctly formatted external events information for CalendarJS, unpublished events only."""
 
+    queryset = ExternalEvent.objects.filter(published=False)
     serializer_class = UnpublishedEventsCalenderJSSerializer
     permission_classes = [IsAdminUser, UnpublishedEventPermissions]
     pagination_class = None
+    filter_backends = (
+        filters.EventDateFilter,
+        filters.CategoryFilter,
+        filters.OrganiserFilter,
+    )
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         context["member"] = self.request.member
         return context
 
-    def get_queryset(self):
-        start, end = extract_date_range(self.request)
-        return Event.objects.filter(end__gte=start, start__lte=end, published=False)
+
+class CalendarJSExternalEventListView(ListAPIView):
+    """Define a custom route that outputs the correctly formatted events information for CalendarJS, published events only."""
+
+    queryset = ExternalEvent.objects.filter(published=True)
+    serializer_class = ExternalEventCalendarJSSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = None
+    filter_backends = (
+        filters.EventDateFilter,
+        filters.CategoryFilter,
+        filters.OrganiserFilter,
+    )
--- website/events/api/v2/serializers/external_event.py
+++ website/events/api/v2/serializers/external_event.py
@@ -4,30 +4,10 @@ from django.utils.html import strip_tags
 from rest_framework import serializers
 
 from events.models.external_event import ExternalEvent
-from partners.models import Partner
 
 
-class PartnerSerializer(serializers.ModelSerializer):
-    """Partner serializer."""
-
-    class Meta:
-        """Meta class for partner serializer."""
-
-        model = Partner
-        fields = (
-            "pk",
-            "name",
-            "link",
-            "company_profile",
-            "address",
-            "zip_code",
-            "city",
-            "logo",
-        )
-
-
-class PartnerEventSerializer(serializers.ModelSerializer):
-    """Partner events serializer."""
+class ExternalEventSerializer(serializers.ModelSerializer):
+    """External events serializer."""
 
     class Meta:
         """Meta class for partner events serializer."""
